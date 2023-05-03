923b03c068bbc7def60c21d9e936d2c824f15071
suezhalder <szhldr@gmail.com>
2023-05-04 02:09:14 +0600

    blog section done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 import { FaApple, FaGoogle } from 'react-icons/fa';
 
 
@@ -7,22 +7,14 @@ const Footer = () => {
     return (
         <Container fluid className="bg-light text-dark py-5 border-top mt-5">
             <Container className=" mx-auto">
-                <Row className="justify-content-center mb-3">
+                <Row className="mb-3">
                     <h3 className="text-center fs-2"> Download our Recipe Book </h3>
                     <p className="text-center"> Discover the secret recipes of our exclusive chefs. </p>
                 </Row>
-                <Row className="d-flex gap-4 w-25 mx-auto my-5">
-                    <div>
-                        <FaGoogle className="me-3 text-center" size={30} />
-                        Download on Google Play
-                    </div>
-                    <div>
-                        <FaApple className='me-3 text-center' size={30} />
-                        Download on App Store
-                    </div>
+                <div className='text-center'>
+                    <Button className='w-25 mx-auto'>Recipe Book</Button>
+                </div>
 
-
-                </Row>
                 <Row className="justify-content-md-between fs-6  mt-5">
                     <Col md={4} className="order-md-2">
                         <span className="px-2">About us</span>
