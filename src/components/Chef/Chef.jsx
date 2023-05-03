1805bacc3de74646dbc27471c4c98c208686aacb
suezhalder <szhldr@gmail.com>
2023-05-04 02:32:23 +0600

    lazy load applied

--- src/components/Chef/Chef.jsx
+++ src/components/Chef/Chef.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Button, Card, Col, Row } from 'react-bootstrap';
-import { FaClock, FaHeart, FaUtensils} from 'react-icons/fa';
+import { FaClock, FaHeart, FaUtensils } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import LazyLoad from 'react-lazy-load';
+
 
 const Chef = ({ chef }) => {
     // console.log(chef);
@@ -11,12 +13,14 @@ const Chef = ({ chef }) => {
     return (
         <Col>
             <Card className="border-0 shadow-lg" >
-                <Card.Img src={picture} style={{ height: '400px', borderRadius: '40px' }} className='p-4' />
+                <LazyLoad height={400} threshold={1}>
+                    <Card.Img src={picture} style={{ height: '400px', borderRadius: '40px' }} className='p-4' />
+                </LazyLoad>
                 <Card.Body className="text-center">
                     <Card.Title className="mb-3 fw-semibold fs-3">{name}</Card.Title>
-                    
+
                     <Card.Text>
-                         <FaClock className='me-2 text-success'></FaClock> Years of experience: {experience} years
+                        <FaClock className='me-2 text-success'></FaClock> Years of experience: {experience} years
                     </Card.Text>
                     <Card.Text>
                         <FaUtensils className='me-2 text-secondary'></FaUtensils> Total Recipes: {total_recipes}
--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,35 +1,50 @@
 import React from 'react';
 import { Accordion } from 'react-bootstrap';
 
-const Blogs = () => {
-    return (
-        <Accordion defaultActiveKey={['0']} alwaysOpen className='w-75 mx-auto mt-5'>
-            <Accordion.Item eventKey="0">
-                <Accordion.Header >Tell us the differences between uncontrolled and controlled components.</Accordion.Header>
-                <Accordion.Body>
-                    In React, form components can be either controlled or uncontrolled. Controlled components rely on React state to manage user input, whereas uncontrolled components rely on DOM references.
-                </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="1">
-                <Accordion.Header >How to validate React props using PropTypes</Accordion.Header>
-                <Accordion.Body>
-                    React provides a way to validate the data types of props that are passed to components using the prop-types package. If a prop is passed to the component that does not match the expected data type or is missing a required prop, a warning will be logged to the console during development.
-                </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="2">
-                <Accordion.Header >Tell us the difference between nodejs and express js.</Accordion.Header>
-                <Accordion.Body>
-                    Node.js is like a toolbox with lots of tools inside. It's used to build JavaScript applications on the server-side. Express.js is like a specialized tool in that toolbox. It's a framework built on top of Node.js that helps developers build web applications more easily.
-                </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="3">
-                <Accordion.Header >What is a custom hook, and why will you create a custom hook?</Accordion.Header>
-                <Accordion.Body>
-                    A custom hook is a function that you can create to reuse some of your code logic in React. It can be used to store or update state, work with effects, or manage other hooks. You might want to create a custom hook to make your code more reusable and easier to understand, or to share logic between different components.
-                </Accordion.Body>
-            </Accordion.Item>
 
-        </Accordion>
+const Blogs = () => {
+    
+
+    return (
+        <div>
+            <div>
+                <Accordion defaultActiveKey={['0']} alwaysOpen className='w-75 mx-auto mt-5'>
+                    <Accordion.Item eventKey="0">
+                        <Accordion.Header >Tell us the differences between uncontrolled and controlled components.</Accordion.Header>
+                        <Accordion.Body>
+                            In React, form components can be either controlled or uncontrolled. Controlled components rely on React state to manage user input, whereas uncontrolled components rely on DOM references.
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="1">
+                        <Accordion.Header >How to validate React props using PropTypes</Accordion.Header>
+                        <Accordion.Body>
+                            React provides a way to validate the data types of props that are passed to components using the prop-types package. If a prop is passed to the component that does not match the expected data type or is missing a required prop, a warning will be logged to the console during development.
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="2">
+                        <Accordion.Header >Tell us the difference between nodejs and express js.</Accordion.Header>
+                        <Accordion.Body>
+                            Node.js is like a toolbox with lots of tools inside. It's used to build JavaScript applications on the server-side. Express.js is like a specialized tool in that toolbox. It's a framework built on top of Node.js that helps developers build web applications more easily.
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="3">
+                        <Accordion.Header >What is a custom hook, and why will you create a custom hook?</Accordion.Header>
+                        <Accordion.Body>
+                            A custom hook is a function that you can create to reuse some of your code logic in React. It can be used to store or update state, work with effects, or manage other hooks. You might want to create a custom hook to make your code more reusable and easier to understand, or to share logic between different components.
+                        </Accordion.Body>
+                    </Accordion.Item>
+
+                </Accordion>
+            </div>
+
+            <div>
+                
+            </div>
+
+            
+        </div>
+
+
     );
 };
 
